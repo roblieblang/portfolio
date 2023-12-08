f989e30554aa1bd0f87f43229fe537430fe15a6d
Robert Lieblang <roberttheanimal01@gmail.com>
2023-12-08 17:23:18 -0600

    Fix "Explore" button

--- src/app/components/sections/hero.tsx
+++ src/app/components/sections/hero.tsx
@@ -8,11 +8,15 @@ export default function HeroSection() {
       className="h-screen position-fixed flex flex-col justify-start items-center text-center mt-24 "
     >
       <h1 className="text-5xl md:text-6xl font-bold">Robert Lieblang</h1>
-      <p className="text-xl md:text-2xl text-text-color mt-4">Software Engineer</p>
-      <button className="mt-8 py-2 px-4 rounded transition flame-text flame-button">
-        <Link href="/#about">Explore</Link>
-      </button>
-    {/* <DigitalPattern /> */}
+      <p className="text-xl md:text-2xl text-text-color mt-4">
+        Software Engineer
+      </p>
+      <Link href="/#about">
+        <button className="mt-8 py-2 px-4 rounded transition flame-text flame-button">
+          Explore
+        </button>
+      </Link>
+      {/* <DigitalPattern /> */}
     </div>
   );
 }
